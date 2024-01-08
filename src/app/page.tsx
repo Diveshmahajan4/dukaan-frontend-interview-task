543c559602dfd4158b9802ce8f0759935507e3c3
DiveshMahajan <diveshmahajan04@gmail.com>
2024-01-09 03:45:31 +0530

    Order List Added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,17 @@
-import {  BadgePercent, BarChartBig, ChevronDown, ClipboardList, CreditCard, HomeIcon, LayoutGrid, MousePointer2, Palette, Truck, Users, Volume2, Zap } from 'lucide-react'
+import OrderList from '@/components/OrderList'
+import {  ArrowDown, ArrowDownToLine, ArrowUpDown, BadgePercent, BarChartBig, ChevronDown, ClipboardList, CreditCard, HelpCircle, HomeIcon, Info, LayoutGrid, MousePointer2, Palette, Search, Truck, Users, Volume2, Zap } from 'lucide-react'
 import Image from 'next/image'
 
 export default function Home() {
+  const ordersToRender = [];
+
+    for (let i = 0; i < 20; i++) {
+      ordersToRender.push(<OrderList key={i} />);
+    }
+
   return (
-    <div className='grid grid-cols-6 bg-white'>
-      <div className='bg-[#353C53] h-100 px-2.5 py-4'>
+    <div className='grid grid-cols-6 bg-white '>
+      <div className='bg-[#353C53] h-100 px-2.5 py-4 text-white'>
           {/* Logo and Name */}
           <div className='flex gap-3'>
             <div>
@@ -72,9 +79,110 @@ export default function Home() {
               </div>
               
           </div>
+
+          {/*  */}
+          <div>
+            
+          </div>
       </div>
 
       {/* Dashboard Navbar*/}
+      <div className='col-span-5'>
+        {/* Navbar */}
+        <div className='grid grid-cols-3 gap-4 py-4 px-8 items-center border-b-2 border-gray-200'>
+            <div className='flex gap-4 w-auto'>
+              <p className='text-[15px] font-md'>Payments</p>
+              <div className='flex items-center gap-2 w-auto'>
+                <HelpCircle className='text-gray-400 w-4 h-4'/>
+                <p className='text-sm font-sm text-gray-400'> How it works?</p>
+              </div>
+            </div>
+            <div className='flex gap-4 w-auto items-center bg-gray-100 px-4 py-2'>
+              <Search className='text-gray-400 w-5 h-5'/>
+              <p className='text-[15px] text-gray-400'>Search features, tutorial, etc.</p>
+            </div>
+            <div className='flex gap-4 w-auto justify-end'>
+              <div className='rounded-full bg-gray-200 h-10 w-10 flex items-center justify-center'>
+              <Image src='./Group.svg' alt="group" width={20} height={20}/>
+              </div>
+              <Image src='./Menu.svg' alt="menu" width={40} height={40}/>
+            </div>
+        </div>
+
+        {/* Main content */}
+        <div className='flex flex-col px-6 py-4'>
+          <div>
+            {/* Overview Layer */}
+            <div className='flex justify-between'>
+              <p className='text-[20px]'>Overview</p>
+              <div className='px-2 py-1 rounded-sm flex border border-gray-300 items-center'>
+                <p className='text-[15px] text-[#4D4D4D]'>Last Month</p>
+                <ChevronDown className='text-gray-400 h-5 w-5'/>
+              </div>
+            </div>
+            {/* Orders Layer */}
+            <div className='grid grid-cols-2 gap-5 mt-6'>
+              <div className='p-5 flex flex-col gap-4 drop-shadow-md'>
+                <div><p className='text-[16px] text-[#4D4D4D]'>Online Orders</p></div>
+                <div><p className='text-4xl'>231</p></div>
+              </div>
+              <div className='p-5 flex flex-col gap-4 drop-shadow-md'>
+                <div><p className='text-[16px] text-[#4D4D4D]'>Amount received</p></div>
+                <div><p className='text-4xl'>₹23,92,312.19</p></div>
+              </div>
+            </div>
+          </div>
+
+          {/* Transactions section */}
+          <div className='mt-8 flex flex-col'>
+            <div className='w-auto'>
+              <p className='text-[20px] font-[500]'>Transactions | This Month</p>
+            </div>
+
+            {/* Order Details */}
+            <div className='mt-5 p-3'>
+                {/* Search Sort and Download Section */}
+                <div className='flex flex-col gap-3'>
+                    <div className='flex gap-3'>
+                      <div className='flex justify-between w-full'>
+                        <div className='flex flex-start py-2.5 px-4 border border-gray-200 rounded-md w-[248px]'>
+                          <Search className='text-gray-400 w-5 h-5'/>
+                          <p className='text-[15px] text-gray-400 pl-2'>Search by order ID...</p>
+                        </div>
+                        {/* Sort Icon */}
+                        <div className='flex flex-start items-center py-2.5 px-4 border border-gray-200 rounded-md '>
+                          <p className='text-[16px] text-[#4D4D4D]'>Sort</p>
+                          <ArrowUpDown className='text-[#4D4D4D] w-5 h-5 pl-1'/>
+                        </div>
+                      </div>
+                      
+                      {/* Download Icon  */}
+                      <div className='flex flex-start items-center py-2.5 px-4 border border-gray-200 rounded-md '>
+                        <ArrowDownToLine className='text-[#4D4D4D] w-5 h-5'/>
+                      </div>
+                    </div>
+
+                    {/* Table Header */}
+                    <div className='py-2.5 px-3 grid grid-cols-4 gap-10 rounded-md bg-[#F2F2F2]'>
+                      <div className='flex justify-start items-start'><p className='text-[15px] font-[500]'>Order ID</p></div>
+                      <div className='flex justify-start items-center'>
+                        <p className='text-[15px] text-[#4D4D4D] font-[500]'>Order Date</p>
+                        <ChevronDown className='text-[#4D4D4D] w-5 h-5 ml-1'/>
+                      </div>
+                      <div className='flex justify-end items-end'><p className='text-[15px] font-[500]'>Order Amount</p></div>
+                      <div className='flex justify-end items-center'>
+                        <p className='text-[15px] text-[#4D4D4D] font-[500]'>Transaction Fees</p>
+                        <Info className='text-[#4D4D4D] w-4 h-4 ml-1'/>
+                      </div>
+                    </div>
+
+                    {/* Orders List */}
+                    {ordersToRender}
+                </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
---
+++ src/components/OrderList.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import SingleOrder from './SingleOrder'
+
+const OrderList = () => {
+  return (
+    <div className='grid grid-cols-4 gap-10 px-3 py-3.5'>
+            <div className='flex justify-start items-start'><p className='text-[15px] font-[500] text-blue-400'>#281209</p></div>
+            <div className='flex justify-start items-center'>
+            <p className='text-[15px] text-[#4D4D4D] font-[500]'>7 July, 2023</p>
+            </div>
+            <div className='flex justify-end items-end'><p className='text-[15px] font-[500]'>&#x20B9;1728.23</p></div>
+            <div className='flex justify-end items-center'>
+                <p className='text-[15px] text-[#4D4D4D] font-[500]'>&#x20B9;22</p>
+            </div>
+    </div>
+  )
+}
+
+export default OrderList
